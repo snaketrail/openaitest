796b580f27e6920836cdacd57300197c6f91cc68
snaketrail <stefdim0211@gmail.com>
2023-08-29 23:20:28 +0300

    iframe swap to normal div
    this works for the website so much better

--- script.js
+++ script.js
@@ -39,11 +39,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-function changeContent(url, button) {
-  var iframe = document.getElementById('content-iframe');
-  iframe.src = url;
+    function changeContent(title, text, imageUrl, button) {
+      var itemTitle = document.getElementById('itemTitle');
+      var itemText = document.getElementById('itemText');
+      var itemImage = document.getElementById('itemImage');
+    
+      var buttons = document.querySelectorAll('.button');
+      buttons.forEach(btn => btn.classList.remove('active')); // Remove active class from all buttons
+      button.classList.add('active'); // Add active class to the clicked button
   
-  var buttons = document.querySelectorAll('.button');
-  buttons.forEach(btn => btn.classList.remove('selected'));
-  button.classList.add('selected');
+      // Update content
+      itemTitle.textContent = title;
+      itemText.textContent = text;
+      itemImage.src = imageUrl;
+      itemImage.alt = title;
+  }
+  
+  function buttonHome1(){
+      changeContent("Website Design", "We create stunning and responsive websites.", "website-design.jpg", event.target);
+  }
+  function buttonHome2(){
+      changeContent("Copywriting", "Compelling content that captures your audience.", "copywriting.jpg", event.target);
+  }
+  function buttonHome3(){
+      changeContent("Graphic Design", "Eye-catching visuals to convey your message.", "graphic-design.jpg", event.target);
+  }
+  function buttonHome4(){
+      changeContent("SEO Audit", "Boost your online presence with our SEO expertise.", "seo-audit.jpg", event.target);
+  }
+  function buttonHome5(){
+      changeContent("Virtual Assistance", "Outsource tasks and focus on what matters.", "virtual-assistance.jpg", event.target);
+  }
+  function buttonHome6(){
+      changeContent("Business Automation", "Streamline your processes for efficiency.", "business-automation.jpg", event.target);
   }
