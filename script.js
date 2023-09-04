0a00ee438b81cf2120e0cf962fe3593dbdcda1ce
snaketrail <stefdim0211@gmail.com>
2023-09-04 10:49:06 +0300

    added footer and modal, also fixed menu active class
    everything works great now

--- script.js
+++ script.js
@@ -7,32 +7,70 @@ window.addEventListener('DOMContentLoaded', () => {
     menuToggle.addEventListener('click', () => {
       menu.classList.toggle('active');
       icon.textContent = menu.classList.contains('active') ? '✕' : iconText;
-      
-
-    //   if (menu.classList.contains('active')) {
-    //     serviceItem.style.zIndex = '-1';
-    //   } else {
-    //     serviceItem.style.zIndex = ''; 
-    //   }
-    // });
 });
 
   
 window.addEventListener('DOMContentLoaded', () => {
   const menuLinks = document.querySelectorAll('.menu a');
-    
+  const currentLocation = window.location.pathname; 
+
   menuLinks.forEach((link) => {
-    link.addEventListener('click', (e) => {
-      menuLinks.forEach((link) => {
-        link.classList.remove('active');
-      });
-      e.target.classList.add('active');
-    });
+    const linkURL = link.getAttribute('href');
+
+    if (currentLocation === linkURL) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
   });
 });
 
+
   
   
-function goToHome(){
-  window.location.href = "index.html";
-}})
+  document.getElementsByClassName("logo")[0].addEventListener("click", function (){
+    window.location.href = "index.html";
+  });
+})
+
+
+
+const privacyPolicyLink = document.getElementById('privacy-policy-link');
+
+
+const modalOverlay = document.getElementById('modal-overlay');
+const modal = document.getElementById('modal');
+
+function openModal() {
+  modalOverlay.style.display = 'block';
+  modal.style.display = 'block';
+}
+
+
+function closeModal() {
+  modalOverlay.style.display = 'none';
+  modal.style.display = 'none';
+}
+
+
+privacyPolicyLink.addEventListener('click', openModal);
+
+
+modalOverlay.addEventListener('click', closeModal);
+
+const tosPolicyLink = document.getElementById('tos-policy-link');
+const modal1Overlay = document.getElementById('modal1-overlay');
+const modal1 = document.getElementById('modal1');
+
+function openModal1() {
+  modal1Overlay.style.display = 'block';
+  modal1.style.display = 'block';
+}
+
+function closeModal1() {
+  modal1Overlay.style.display = 'none';
+  modal1.style.display = 'none';
+}
+
+tosPolicyLink.addEventListener('click', openModal1);
+modal1Overlay.addEventListener('click', closeModal1);
